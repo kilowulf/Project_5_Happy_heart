d60c3fb809573876d28a3b6eb1504b4092fb75d7
Jon <jonnydc4@gmail.com>
2023-10-03 20:40:00 -0600

    New test classes

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import unittest
 
-
+'''
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+'''
 
-    # This is a test comment
-    # This is another test comment
+class Test_Case_PulseAlarm(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class Test_Case_OxygenAlarm(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class Test_Case_BloodPressureAlarm(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
