f33c9f5d5d8e566804d84241e2439d19198ba4ef
DallinM1 <112734105+DallinM1@users.noreply.github.com>
2023-10-03 20:06:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import time
 import sys
 from collections import deque
+from abc import ABC, abstractmethod
 
 
-def check_alarms(pulse, oxygen_avg, bp_s, bp_d):
-    alarm = "None"
-    message = "Everything normal"
+class BaseAlarm(ABC):
+    @abstractmethod
+    def check(self, *args):
+        pass
 
-    if pulse is not None:
+
+class PulseAlarm(BaseAlarm):
+    def check(self, pulse):
+        if pulse is None:
+            return "None", "Everything normal"
         if pulse < 20:
             return "Highest", "Pulse dangerously low"
         if pulse < 40:
@@ -18,16 +24,26 @@ def check_alarms(pulse, oxygen_avg, bp_s, bp_d):
             return "Medium", "Pulse too high"
         if pulse > 110:
             return "Low", "Pulse elevated"
+        return "None", "Everything normal"
 
-    if oxygen_avg is not None:
+
+class OxygenAlarm(BaseAlarm):
+    def check(self, oxygen_avg):
+        if oxygen_avg is None:
+            return "None", "Everything normal"
         if oxygen_avg < 50:
             return "Highest", "Oxygen level dangerously low"
         if oxygen_avg < 80:
             return "Medium", "Oxygen level too low"
         if oxygen_avg < 85:
             return "Low", "Oxygen level slightly low"
+        return "None", "Everything normal"
 
-    if bp_s is not None and bp_d is not None:
+
+class BloodPressureAlarm(BaseAlarm):
+    def check(self, bp_s, bp_d):
+        if bp_s is None or bp_d is None:
+            return "None", "Everything normal"
         if bp_s > 200 or bp_d > 120:
             return "Medium", "Blood pressure dangerously high"
         if bp_s > 150 or bp_d > 90:
@@ -36,8 +52,31 @@ def check_alarms(pulse, oxygen_avg, bp_s, bp_d):
             return "Medium", "Blood pressure too low"
         if bp_s < 50 or bp_d < 33:
             return "Highest", "Blood pressure dangerously low"
+        return "None", "Everything normal"
 
-    return alarm, message
+
+class AlarmSystem:
+    def __init__(self):
+        self.alarms = {
+            "pulse": PulseAlarm(),
+            "oxygen": OxygenAlarm(),
+            "bp": BloodPressureAlarm()
+        }
+
+    def check_all(self, pulse, oxygen_avg, bp_s, bp_d):
+        alarm, message = self.alarms["pulse"].check(pulse)
+        if alarm != "None":
+            return alarm, message
+
+        alarm, message = self.alarms["oxygen"].check(oxygen_avg)
+        if alarm != "None":
+            return alarm, message
+
+        alarm, message = self.alarms["bp"].check(bp_s, bp_d)
+        if alarm != "None":
+            return alarm, message
+
+        return "None", "Everything normal"
 
 
 def read_data_source(data_source):
@@ -74,7 +113,9 @@ def main():
     else:
         data_source = sys.stdin
 
-    oxygen_deque = deque(maxlen=6)  # For 1 minute moving average (6 x 10 seconds)
+    alarm_system = AlarmSystem()
+
+    oxygen_deque = deque(maxlen=6)
     missing_oxygen_count = 0
     time_elapsed = 0
 
@@ -94,7 +135,7 @@ def main():
         else:
             oxygen_avg = sum(oxygen_deque) / len(oxygen_deque) if len(oxygen_deque) > 0 else None
 
-        alarm, message = check_alarms(pulse, oxygen_avg, bp_s, bp_d)
+        alarm, message = alarm_system.check_all(pulse, oxygen_avg, bp_s, bp_d)
 
         print(f"{time.strftime('%M:%S', time.gmtime(time_elapsed))} {alarm} {message}")
 
